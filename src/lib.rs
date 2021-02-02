ae11143a2fed6abad6a51a5dabc4fa10fe190079
Justin Boswell <boswej@amazon.com>
2021-02-02 11:34:33 -0800

    Added basic lifetime unit test

--- src/lib.rs
+++ src/lib.rs
@@ -17,3 +17,38 @@ impl TestStruct {
         return "RETURN_STR".as_ptr();
     }
 }
+
+#[allow(dead_code)]
+extern "C" {
+    pub fn aws_crt_init();
+    pub fn aws_crt_clean_up();
+}
+
+pub struct CRT {
+
+}
+
+impl CRT {
+    pub fn init() {
+        unsafe {
+            aws_crt_init();
+        }
+    }
+
+    pub fn clean_up() {
+        unsafe {
+            aws_crt_clean_up();
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn init_and_clean_up() {
+        CRT::init();
+        CRT::clean_up();
+    }
+}
